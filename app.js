9431e4107fe80e54d0adc208d767b31ffb381d26
ALATHA <Nomthandazomde@gmail.com>
2016-02-16 15:39:46 +0200

    taxi kata

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-
-var body = document.querySelector("body");
+var body = document.querySelector("body")
 
 body.onkeydown = function(e){
-    //e.keyCode - will capture the key codes
+   // ? e.keyCode - will capture the key codes
+   var displayMessage=function(keyCode){
+   	displayMessage(e.keyCode);
+   }
+};
+var keyCodeName=function(keycode){
+if(keycode===37){
+	return "left";
+}
+if(keycode===38){
+	return "up";
+}
+if(keycode===39){
+	return "right";
+}
+if(keycode===40){
+	return "down";
+}
+else {
+	return "";
+}
 };
