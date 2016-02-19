3ddbff97cde6ec7864f854123cd5bde5f06ba7d5
ALATHA <Nomthandazomde@gmail.com>
2016-02-19 14:58:15 +0200

    modified

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //find the body tag and store it in a variable called 'body'
-var body = document.querySelector("body")
-
+var body = document.querySelector("body");
+var taxiLocationCounter=1;
 //listen for the keydown event
 body.onkeydown = function(e){
    // ? e.keyCode - will capture the key codes
@@ -9,53 +9,86 @@ body.onkeydown = function(e){
  	var myClass=createLocationClass(taxiLocationCounter);
  	displayMessage(myClass);
 
+ 	if(keyName==="right"){
+ 		moveForward();
+ 	}
+ 	else if(keyName==="left"){
+ 		moveBackwards();
+ 
+ 	}
  };
 
-var taxiLocationCounter=1;
+
+
+
 	  var i=0;
 	  i=i+1;
 	 if(keyCodeName==="right"){
-		taxiLocationCounter=taxiLocationCounter+1;
+		taxiLocationCounter=taxiLocationCounter++;
 	}
 	else if(keyCodeName==="left"){
-		taxiLocationCounter=taxiLocationCounter-1;
+		taxiLocationCounter=taxiLocationCounter--;
 	}
 
 var createLocationClass=function(number){
 		if(number===1){
 			return "slot-one-of-nine";
 		}
-		if(number===5){
+		else if(number===2){
+			return "slot-two-of-nine";
+		}
+		else if(number===3){
+			return "slot-three-of-nine";
+		}
+		if(number===4){
+			return "slot-four-of-nine";
+		}
+		else if(number===5){
 			return "slot-five-of-nine";
 		}
-		if(number===9){
-			return "slot-nine-of-nine";
+		else if(number===6){
+			return "slot-six-of-nine";
+			}
+			if(number===7){
+			return "slot-seven-of-nine";
 		}
-	}
+		else if(number===8){
+			return "slot-eight-of-nine";
+		}
+		else if(number===9){
+			return "slot-nine-of-nine";
+	};
+}
 
 var keyCodeName=function(keycode){
 if(keycode===37){
 	return "left";
 }
-if(keycode===38){
+else if(keycode===38){
 	return "up";
 }
-if(keycode===39){
+else if(keycode===39){
 	return "right";
 }
-if(keycode===40){
+ else if(keycode===40){
 	return "down";
 }
 else {
 	return "";
-	};
+	}
 };
 
  function moveForward(){
 	var currentLocation = createLocationClass(taxiLocationCounter);
-	taxiLocationCounter+1;
+	taxiLocationCounter++;
 	var newLocation = createLocationClass(taxiLocationCounter);
 	moveTaxi(currentLocation,newLocation);
 
+};
+function moveBackwards(){
+	var currentLocation = createLocationClass(taxiLocationCounter);
+	taxiLocationCounter--;
+	var newLocation = createLocationClass(taxiLocationCounter);
+	moveTaxi(currentLocation,newLocation);
 };
 
